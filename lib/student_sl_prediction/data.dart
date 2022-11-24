f55ea9a9b46fce1797495f93e0265cd0422d32e2
Supritha0203 <102866536+Supritha0203@users.noreply.github.com>
2022-11-24 17:31:53 +0530

    Create data.dart

---
+++ lib/student_sl_prediction/data.dart
@@ -0,0 +1,360 @@
+class Data {
+  static const List<Map<String,dynamic>> questionsFiveAndSix = [
+    {
+      'questionText': ' What animal hibernates during the winter? ',
+      'answers': [
+        {'text': 'Wolf', 'score': 3},
+        {'text': 'Tiger', 'score': 5},
+        {'text': 'bear', 'score': 6},
+      ],
+    },
+    {
+      'questionText':
+          ' If you rearrange the letters "RIGET", you would have the name of a: ',
+      'answers': [
+        {'text': 'Ocean', 'score': 3},
+        {'text': 'Animal', 'score': 5},
+        {'text': 'Country', 'score': 6},
+      ],
+    },
+    {
+      'questionText': ' Which word does not contain letter «D»? ',
+      'answers': [
+        {'text': 'Mouse', 'score': 9},
+        {'text': 'Dog', 'score': 8},
+        {'text': 'HedgeHog', 'score': 7},
+      ],
+    },
+    {
+      'questionText':
+          '  Find the musical instrument that you play by blowing air:  ',
+      'answers': [
+        {'text': 'Guitar', 'score': 9},
+        {'text': 'Drum', 'score': 8},
+        {'text': 'Violin', 'score': 7},
+        {'text': 'Trumpet', 'score': 6},
+        {'text': 'Piano', 'score': 5},
+      ],
+    },
+    {
+      'questionText':
+          '  Which one of the five is least like the other four? Tulip, Lily, Tomato, Chamomile, Violet ',
+      'answers': [
+        {'text': 'tulip', 'score': 9},
+        {'text': 'Lily', 'score': 8},
+        {'text': 'tomato', 'score': 7},
+        {'text': 'chamomile', 'score': 6},
+        {'text': 'violet', 'score': 5},
+      ],
+    },
+    {
+      'questionText':
+          'Hedgehog is weaker than a wolf. But hedgehog is stronger than a mouse. Who is the weakest? ',
+      'answers': [
+        {'text': 'Hedgehog', 'score': 9},
+        {'text': 'Mouse', 'score': 8},
+        {'text': 'Wolf', 'score': 7},
+      ],
+    },
+    {
+      'questionText':
+          'Which one of the four is least like the other three? February, March, April, May ',
+      'answers': [
+        {'text': 'february', 'score': 9},
+        {'text': 'march', 'score': 8},
+        {'text': 'april', 'score': 7},
+        {'text': 'may', 'score': 6},
+      ],
+    },
+    {
+      'questionText':
+          ' If July 3rd is a Tuesday, which day of the week would be July 6th? ',
+      'answers': [
+        {'text': 'Wednesday', 'score': 9},
+        {'text': 'Friday', 'score': 8},
+        {'text': 'Saturday', 'score': 7},
+      ],
+    },
+    {
+      'questionText': ' Which of these animals is the largest?  ',
+      'answers': [
+        {'text': 'Elephant', 'score': 9},
+        {'text': 'Tiger', 'score': 8},
+        {'text': 'Crow', 'score': 7},
+        {'text': 'Crow', 'score': 6},
+        {'text': 'Crow', 'score': 5},
+      ],
+    },
+  ];
+
+  static const List<Map<String,dynamic>> questionsSevenAndEight = [
+    {
+      'questionText': '  How many sides does a hexagon have? ',
+      'answers': [
+        {'text': '3', 'score': 3},
+        {'text': '4', 'score': 5},
+        {'text': '5', 'score': 6},
+        {'text': '6', 'score': 7},
+      ],
+    },
+    {
+      'questionText': '  It is 5:10 pm now. What time it was 30 minutes ago? ',
+      'answers': [
+        {'text': '4:20', 'score': 3},
+        {'text': '4:30', 'score': 5},
+        {'text': '4:40', 'score': 6},
+        {'text': '5:40', 'score': 4},
+        {'text': '5:20', 'score': 2},
+      ],
+    },
+    {
+      'questionText':
+          'Hedgehog is weaker than a wolf. But hedgehog is stronger than a mouse. Who is the weakest?',
+      'answers': [
+        {'text': 'Hedgehog', 'score': 8},
+        {'text': 'Mouse', 'score': 9},
+        {'text': 'Wolf', 'score': 7},
+      ],
+    },
+    {
+      'questionText':
+          ' If you rearrange the letters "KOMENY", you would have the name of a:  ',
+      'answers': [
+        {'text': 'Ocean', 'score': 8},
+        {'text': 'Animal', 'score': 9},
+        {'text': 'Country', 'score': 7},
+      ],
+    },
+    {
+      'questionText': ' What color do you get when you mix yellow and blue?  ',
+      'answers': [
+        {'text': 'red', 'score': 7},
+        {'text': 'white', 'score': 8},
+        {'text': 'green', 'score': 9},
+        {'text': 'brown', 'score': 6},
+        {'text': 'pink', 'score': 5},
+      ],
+    },
+    {
+      'questionText':
+          'Hedgehog is weaker than a wolf. But hedgehog is stronger than a mouse. Who is the weakest? ',
+      'answers': [
+        {'text': 'Hedgehog', 'score': 9},
+        {'text': 'Mouse', 'score': 8},
+        {'text': 'Wolf', 'score': 7},
+      ],
+    },
+    {
+      'questionText': ' To pound a nail, you need a: ',
+      'answers': [
+        {'text': 'scissors', 'score': 7},
+        {'text': 'knife', 'score': 8},
+        {'text': 'hammer', 'score': 9},
+        {'text': 'shovel', 'score': 6},
+        {'text': 'saw', 'score': 5},
+      ],
+    },
+    {
+      'questionText':
+          ' If July 3rd is a Tuesday, which day of the week would be July 6th? ',
+      'answers': [
+        {'text': 'Wednesday', 'score': 7},
+        {'text': 'Friday', 'score': 8},
+        {'text': 'Saturday', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'Which one of the four is least like the other three? February, March, April, May ',
+      'answers': [
+        {'text': 'february', 'score': 6},
+        {'text': 'march', 'score': 8},
+        {'text': 'april', 'score': 7},
+        {
+          'text': 'may',
+          'score': 9,
+        }
+      ],
+    },
+  ];
+
+  static const List<Map<String,dynamic>> questionsNineAndTen = [
+    {
+      'questionText': ' Which word disrupts the ordering? Car, Tree,Table, Furniture, Coconut ',
+      'answers': [
+        {'text': 'coconut', 'score': 3},
+        {'text': 'furniture', 'score': 5},
+        {'text': 'table', 'score': 6},
+         {'text': 'car', 'score': 4},
+      ],
+    },
+    {
+      'questionText':
+          'Which number should come to the blank? 1, 5, 9, 13, 17, ? ',
+      'answers': [
+        {'text': '19', 'score': 3},
+        {'text': '20', 'score': 5},
+        {'text': '21', 'score': 6},
+         {'text': '22', 'score': 4},
+      ],
+    },
+    {
+      'questionText': ' Which word does not contain letter «D»? ',
+      'answers': [
+        {'text': 'Mouse', 'score': 7},
+        {'text': 'Dog', 'score': 8},
+        {'text': 'HedgeHog', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'Which is different? Hat, Pen, Car, Cat ',
+      'answers': [
+        {'text': 'hat', 'score': 6},
+        {'text': 'car', 'score': 8},
+        {'text': 'pen', 'score': 7},
+        {'text': 'cat', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          '  Which is different?Dog, Tiger, Sheep, Whale ',
+      'answers': [
+        {'text': 'dog', 'score': 6},
+        {'text': 'tiger', 'score': 8},
+        {'text': 'sheep', 'score': 7},
+        {'text': 'whale', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'What number should come to the blank? A-1, B-2, C-?, D-4 ',
+      'answers': [
+        {'text': '3', 'score': 6},
+        {'text': '4', 'score': 8},
+        {'text': '5', 'score': 7},
+        {'text': '6', 'score': 7},
+      ],
+    },
+    {
+      'questionText':
+          'What number should come to the blank? ABC-347, BC-47, AC-?',
+      'answers': [
+        {'text': '34', 'score': 6},
+        {'text': '37', 'score': 8},
+        {'text': '73', 'score': 7},
+        {'text': '74', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          ' If July 3rd is a Tuesday, which day of the week would be July 6th? ',
+      'answers': [
+        {'text': 'Wednesday', 'score': 9},
+        {'text': 'Friday', 'score': 8},
+        {'text': 'Saturday', 'score': 7},
+      ],
+    },
+    {
+      'questionText': 'What number should come to the blank? 2,5,8,11,14,?    ',
+      'answers': [
+        {'text': '17', 'score': 6},
+        {'text': '18', 'score': 8},
+        {'text': '19', 'score': 7},
+        {'text': '20', 'score': 9},
+      ],
+    },
+  ];
+
+  static const List<Map<String,dynamic>> questionsElevenAndTwelve = [
+    {
+      'questionText': 'Which number logically follows this series? 3, 7,12,18, 25, 33, 42,? ',
+      'answers': [
+        {'text': '48', 'score': 3},
+        {'text': '50', 'score': 5},
+        {'text': '52', 'score': 6},
+         {'text': '54', 'score': 4},
+      ],
+    },
+    {
+      'questionText':
+          'What will be the next number ? 32,16,8,4,2 ? ',
+      'answers': [
+        {'text': '4', 'score': 3},
+        {'text': '3', 'score': 5},
+        {'text': '2', 'score': 6},
+         {'text': '1', 'score': 4},
+      ],
+    },
+    {
+      'questionText': ' Which word does not contain letter «D»? ',
+      'answers': [
+        {'text': 'Mouse', 'score': 7},
+        {'text': 'Dog', 'score': 8},
+        {'text': 'HedgeHog', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'Which is different? Hat, Pen, Car, Cat ',
+      'answers': [
+        {'text': 'hat', 'score': 6},
+        {'text': 'car', 'score': 8},
+        {'text': 'pen', 'score': 7},
+        {'text': 'cat', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          ' What is the value of d ? ABCD= 3257 , abcd:214? ',
+      'answers': [
+        {'text': '6', 'score': 6},
+        {'text': '5', 'score': 8},
+        {'text': '4', 'score': 7},
+        {'text': '3', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'There are 25 books on the table, and you took ten. How many do you have',
+      'answers': [
+        {'text': '35', 'score': 6},
+        {'text': '25', 'score': 8},
+        {'text': '15', 'score': 7},
+        {'text': '10', 'score': 7},
+      ],
+    },
+    {
+      'questionText':
+          'What number should come to the blank? ABC-347, BC-47, AC-?',
+      'answers': [
+        {'text': '34', 'score': 6},
+        {'text': '37', 'score': 8},
+        {'text': '73', 'score': 7},
+        {'text': '74', 'score': 9},
+      ],
+    },
+    {
+      'questionText':
+          'Which one of the letters does not belong in the following series? A .C  F .J .O . ? ',
+      'answers': [
+        {'text': 'S', 'score': 9},
+        {'text': 'T', 'score': 8},
+        {'text': 'U', 'score': 7},
+        {'text': 'X', 'score': 7},
+
+      ],
+    },
+    {
+      'questionText': 'Which word logically follows this series? Eat,Boat,Water, Jacket, puzzle, ? ',
+      'answers': [
+        {'text': ' Accept  ', 'score': 6},
+        {'text': 'Acrobat    ', 'score': 8},
+        {'text': 'Actually', 'score': 7},
+        {'text': 'Activity ', 'score': 9},
+      ],
+    },
+  ];
+
+
+}
